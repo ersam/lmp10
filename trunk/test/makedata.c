5f939ee328d162e0fa6d3feb36505a300717bd32
Eryk Samulak <samulake@len.iem.pw.edu.pl>
2015-01-13 22:04:06 +0100

    Ostateczna wersja projektu po poprawkach estetycznych i testowaniu roznych danych

--- trunk/test/makedata.c
+++ trunk/test/makedata.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX 7
+#define MIN 6
+
 int main()
 {
 	FILE *f1 = fopen("dane2", "w"), *f2 = fopen("dane3", "w");
 	int i, j;
 
-	for ( i = 0; i < 100; i++ ) {
-		fprintf(f1,"%d %g\n", i, 1.0*i*rand()/RAND_MAX);
-		fprintf(f2,"%g %g\n", 0.5*i, -1.2*i*rand()/RAND_MAX);
+	for ( i = 2; i < 50; i++ ) {
+		fprintf(f1,"%g %g\n", 0.1*i, 1.0*i*rand()/RAND_MAX);
+		fprintf(f2,"%g %g\n", 0.1*i, -1.2*i*rand()/RAND_MAX);
 	}
 
 	return 0;
